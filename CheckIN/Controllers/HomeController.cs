b5535d6ee225ff6aebf60700e7274a7bdfe0abc8
Stelian Raev <40591640+stelianraev@users.noreply.github.com>
2024-04-20 16:54:04 +0300

    Add files via upload

---
+++ CheckIN/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using CheckIN.Models;
+using CheckIN.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CheckIN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ITiToService _tiToService;
+
+        public HomeController(ILogger<HomeController> logger, ITiToService tiToService)
+        {
+            _tiToService = tiToService;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CheckIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProcessQRData([FromBody] QRCodeDataModel data)
+        {
+            string qrCodeData = data.QRCodeData;
+            //var tickets = await _tiToService.GetTicket(qrCodeData);
+            var tickets = await _tiToService.GetTickets(qrCodeData);
+           
+            return Ok();
+        }
+
+        public IActionResult Registrations()
+        {
+            // Add logic for handling registrations here
+            return View();
+        }
+    }
+
+    public class QRCodeDataModel
+    {
+        public string QRCodeData { get; set; }
+    }
+}
---
+++ CheckIN/Models/TITo/TitoTicket.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CheckIN.Models.TITo
+{
+    public class TitoTicket
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("slug")]
+        public string Slug { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phone_number")]
+        public int PhoneNumber { get; set; }
+
+        [JsonPropertyName("company_name")]
+        public string CompanyNme { get; set; }
+
+        [JsonPropertyName("release_title")]
+        public string ReleaseTitle { get; set; }
+
+        [JsonPropertyName("reference")]
+        public string Reference { get; set; }
+
+        [JsonPropertyName("registration_reference")]
+        public string RegistrationReference { get; set; }
+
+        [JsonPropertyName("tags")]
+        public string Tags { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public string UpdatedAt { get; set; }
+    }
+}
